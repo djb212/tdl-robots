8b449b18e19596bb6979f384ed17d95708aa1a4e
djb212 <djbaker@cantab.net>
2026-09-18 13:50:16 +0100

    Add unit tests

---
+++ tdl-robots-test/InputParserTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tdl_robots.Data;
+using tdl_robots.Model;
+
+namespace tdl_robots_test
+{
+    internal class InputParserTests
+    {
+        InputParser inputParser;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Arrange
+            inputParser = new InputParser();
+        }
+
+        [Test]
+        public void Given_ValidInput_ParseInput_Returns_CorrectResult()
+        {
+            // Act
+            RobotsInput result = inputParser.ParseInput("input/validtestinput.txt");
+            
+            // Assert
+            Assert.That(result.xBound, Is.EqualTo(5));
+            Assert.That(result.yBound, Is.EqualTo(3));
+
+            Assert.That(result.robots.Count, Is.EqualTo(3));
+            Assert.That(result.robots[2].instructions, Is.EqualTo("LLFFFLFLFL"));
+            Assert.That(result.robots[2].position.x, Is.EqualTo(0));
+            Assert.That(result.robots[2].position.y, Is.EqualTo(3));
+            Assert.That(result.robots[2].position.direction, Is.EqualTo(3));
+        }
+
+        [TestCase("input/doesnotexist.txt", typeof(FileNotFoundException))]
+        [TestCase("input/boundstoobig.txt", typeof(ArgumentOutOfRangeException))]
+        [TestCase("input/startoutofbounds.txt", typeof(ArgumentOutOfRangeException))]
+        [TestCase("input/invalidstartdirection.txt", typeof(FormatException))]
+        [TestCase("input/toomanycoordinates.txt", typeof(FormatException))]
+        public void Given_InvalidInput_ParseInput_Throws_Exception(string file, Type exceptionType)
+        {
+            // Act & Assert
+            Assert.Throws(exceptionType, () => inputParser.ParseInput(file));
+        }
+    }
+}
---
+++ tdl-robots-test/ProcessRobotsTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using tdl_robots.Model;
+using tdl_robots.Services;
+
+namespace tdl_robots_test
+{
+    internal class ProcessRobotsTests
+    {
+        private RobotsInput input;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Arrange
+            input = new RobotsInput
+            {
+                xBound = 5,
+                yBound = 3,
+                robots = new List<Robot>()
+            };
+
+        }
+
+        [TestCase(1, 1, 1, "RFRFRFRF", "1 1 E")]
+        [TestCase(3, 2, 0, "FRRFLLFFRRFLL", "3 3 N LOST")]
+        [TestCase(0, 3, 3, "LLFFFLFLFL", "3 3 N LOST")]
+        public void Given_ValidInput_ProcessRobots_Returns_CorrectResult(int x, int y, int direction, string instructions, string expectedOutput)
+        {
+            // Arrange
+            input.robots.Add(new Robot(x, y, direction, instructions));
+
+            ProcessRobots processRobots = new ProcessRobots(input);
+
+            // Act
+            string[] result = processRobots.Run();
+
+            // Assert
+            Assert.That(result, Has.Length.EqualTo(1));
+            Assert.That(result[0], Is.EqualTo(expectedOutput));
+        }
+
+        [Test]
+        public void Given_LostRobot_ProcessRobots_Returns_RobotNotLostAtSameSquare()
+        {
+            // Arrange
+            input.robots.Add(new Robot(3, 2, 0, "FRRFLLFFRRFLL")); // This robot will be lost
+            input.robots.Add(new Robot(0, 3, 3, "LLFFFLFLFL")); // This robot should not be lost
+
+            ProcessRobots processRobots = new ProcessRobots(input);
+
+            // Act
+            string[] result = processRobots.Run();
+
+            // Assert
+            Assert.That(result, Has.Length.EqualTo(2));
+            Assert.That(result[0], Is.EqualTo("3 3 N LOST"));
+            Assert.That(result[1], Is.EqualTo("2 3 S"));
+        }
+
+        [Test]
+        public void Given_InvalidInstruction_ProcessRobots_Throws_Exception()
+        {
+            // Arrange
+            input.robots.Add(new Robot(3, 2, 0, "TURNIP"));
+
+            ProcessRobots processRobots = new ProcessRobots(input);
+
+            // Act & Assert
+            Exception ex = Assert.Throws<FormatException>(() => processRobots.Run());
+            Assert.That(ex.Message, Is.EqualTo("Invalid instruction character"));
+        }
+
+    }
+}
